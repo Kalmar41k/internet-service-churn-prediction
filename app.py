d9003aa75aafa6aad4c11643c67f0b961abfb425
Kalmar41k <vitalik01082003@gmail.com>
2025-02-24 14:44:10 +0200

    edited file add.py

--- app.py
+++ app.py
@@ -3,26 +3,29 @@ import pickle
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+import joblib
 from sklearn.preprocessing import StandardScaler
 
-X_train = pd.read_csv("X_train.csv")
+X_train = pd.read_csv("data/X_train.csv")
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 
 models = {
-    "Logistic Regression": "models/logistic_regression.pkl",
-    "Random Forest": "models/random_forest.pkl",
-    "Gradient Boosting": "models/gradient_boosting.pkl",
-    "Neural Network": "models/neural_network.h5"
+    "Logistic Regression": "models/logistic_regression_model.pkl",
+    "Random Forest": "models/random_forest_model.pkl",
+    "SVM": "models/best_svm_model.pkl",
+    "Neural Network": "models/dnn_model.h5"
 }
 
 loaded_models = {}
 for name, path in models.items():
-    if path.endswith(".pkl"):
+    if name == "Neural Network":
+        loaded_models[name] = tf.keras.models.load_model(path)
+    elif name == "SVM":
+        loaded_models[name] = joblib.load(path)
+    else:
         with open(path, "rb") as f:
             loaded_models[name] = pickle.load(f)
-    elif path.endswith(".h5"):
-        loaded_models[name] = tf.keras.models.load_model(path)
 
 st.title("Прогнозування відтоку клієнтів")
 selected_model = st.selectbox("Оберіть модель", list(models.keys()))
@@ -33,19 +36,19 @@ for column in X_train.columns:
     user_input[column] = st.number_input(f"{column}", value=0.0)
 
 if st.button("Зробити прогноз"):
-    input_data = np.array([list(user_input.values())]).astype(float)
+    input_data = pd.DataFrame([user_input], columns=X_train.columns)
 
     if selected_model == "Random Forest":
-        transformed_data = input_data
+        transformed_data = input_data.values
     else:
         transformed_data = scaler.transform(input_data)
 
     model = loaded_models[selected_model]
 
     if selected_model == "Neural Network":
-        prediction = model.predict(transformed_data)
-        probability = prediction[0, 0]
-        predicted_class = int(probability > 0.5)
+        prediction = model.predict(transformed_data)[0, 0]
+        predicted_class = int(prediction > 0.5)
+        probability = prediction if predicted_class == 1 else (1 - prediction)
     else:
         predicted_class = model.predict(transformed_data)[0]
         probability = model.predict_proba(transformed_data)[0][predicted_class]
